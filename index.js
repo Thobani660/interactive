2c47fb2d622cbcfe246117ecf50b1193bd47b9b8
User <User@WRSD-LC310.dut.ac.za>
2023-11-27 10:06:01 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+
+// 
+
+
+// ]
+
+function Submit(){
+    document.getElementsByClassName("A")[0].style.display = "none"
+    document.getElementsByClassName("A2")[0].style.display = "block"
+
+
+}
+
+// function bt(){
+// // document.getElementsByClassName("bt")[0].innerHTML.A
+
+// }
+
+function bt1(){
+    document.getElementsByClassName('btnA2')[0].innerHTML = " you selected 1 out  of 5"
+    
+    document.getElementsByClassName("bt1")[0].style.background = "orange"
+    document.getElementsByClassName("bt2")[0].style.color = "grey"
+    document.getElementsByClassName("bt3")[0].style.color = "grey"
+    document.getElementsByClassName("bt4")[0].style.color = "grey"
+    document.getElementsByClassName("bt5")[0].style.color = "grey"
+    console.log("You selected 1 out of 5")
+
+}
+function bt2(){
+    document.getElementsByClassName('btnA2')[0].innerHTML = " you selected 2 out of 5"
+   
+    document.getElementsByClassName("bt1")[0].style.color = "grey"
+    document.getElementsByClassName("bt2")[0].style.background = "orange"
+    document.getElementsByClassName("bt3")[0].style.color = "grey"
+    document.getElementsByClassName("bt4")[0].style.color = "grey"
+    document.getElementsByClassName("bt5")[0].style.color = "grey"
+    console.log("You selected 2 out of 5")
+
+
+}
+function bt3(){
+    document.getElementsByClassName('btnA2')[0].innerHTML = " you selected 3 out of 5"
+ 
+    document.getElementsByClassName("bt1")[0].style.color = "grey"
+    document.getElementsByClassName("bt2")[0].style.color = "grey"
+    document.getElementsByClassName("bt3")[0].style.background = "orange"
+    document.getElementsByClassName("bt4")[0].style.color = "grey"
+    document.getElementsByClassName("bt5")[0].style.color = "grey"
+    console.log("You selected 3 out of 5")
+
+}
+function bt4(){
+    document.getElementsByClassName('btnA2')[0].innerHTML = " you selected 4 out  of 5"
+ 
+    document.getElementsByClassName("bt1")[0].style.color = "grey"
+    document.getElementsByClassName("bt2")[0].style.color = "grey"
+    document.getElementsByClassName("bt3")[0].style.color = "grey"
+    document.getElementsByClassName("bt4")[0].style.background = "orange"
+    document.getElementsByClassName("bt5")[0].style.color = "grey"
+    console.log("You selected 4 out of 5")
+
+}
+function bt5(){
+    document.getElementsByClassName('btnA2')[0].innerHTML = " you selected 5 out of 5"
+ 
+    document.getElementsByClassName("bt1")[0].style.color = "grey"
+    document.getElementsByClassName("bt2")[0].style.color = "grey"
+    document.getElementsByClassName("bt3")[0].style.color = "grey"
+    document.getElementsByClassName("bt4")[0].style.color = "grey"
+    document.getElementsByClassName("bt5")[0].style.background = "orange"
+    console.log("You selected 5 out of 5")
+
+}
+// }
+
+// var block = [{1},{2},{3},{4},{5}]
+// for(var i = 0;  i < time.length; i++){
+     
+//     document.getElementById("c" +i).innerHTML= block[i] + "out of 5"
+//   }
+
+
+// const btn = ["one", "two" , "three", "four"]
+
+// document.getElementsByClassName("bt")
